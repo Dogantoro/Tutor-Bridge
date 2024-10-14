03fa54cbd4fc66519881920e57936a01014a94c8
dogan <dogantoro@gmail.com>
2024-10-14 00:38:55 -0400

    Spring init

---
+++ src/test/java/com/tutorbridge/TutorBridge/TutorBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorbridge.TutorBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
