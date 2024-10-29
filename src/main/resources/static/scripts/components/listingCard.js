8c1c91339494dc84b9601d3f9580fff4aec5a03b
emma-coronado <emma.dz.coronado@gmail.com>
2024-10-28 23:39:18 -0400

    edit file paths

---
+++ src/main/resources/static/scripts/components/listingCard.js
@@ -0,0 +1,36 @@
+
+function ListingCard (props) {
+
+    return (
+        <div>
+            <div class="card listing-card m-3">
+                <div class="card-body">
+                    <h5 class="card-title">{props.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{props.subjects}</h6>
+                    <p class="card-text">{props.bio}</p>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target={"#" + props.id}>
+                    More
+                    </button>
+                </div>
+            </div>
+
+            <div class="modal fade" id={props.id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">{props.name}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
--- src/main/resources/static/scripts/listings.js
+++ src/main/resources/static/scripts/listings.js
@@ -6,9 +6,26 @@ const App = () => {
   return (
     <div>
       <NavBar/>
+      <FormattedListings/>
     </div>
   );
 };
 
 // Render contents
 root.render(<App />);
+
+// cards = [<ListingCard name="Jane Doe" subjects="prog 1, chemistry" bio="This is some stuff about me :)" id='1'/>,
+//         <ListingCard name="Emma" subjects="DSA" bio="This is some stuff about me :)" id='2'/>];
+
+// function FormattedListings (props) {
+//   var toReturn = <div id="listings">;
+//    i = 0;
+//   for (i in props.cards) {
+//     toReturn += <div class="col-3"> {props.cards[i]} </div>
+
+//   }
+
+//   toReturn += </div>;
+
+//   return toReturn;
+// }
