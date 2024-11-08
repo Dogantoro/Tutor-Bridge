205896be8ec815d6c947449643b88e993a619b44
emma-coronado <emma.dz.coronado@gmail.com>
2024-11-08 12:05:55 -0500

    clean up nav styling

--- src/main/resources/static/scripts/components/navbar.js
+++ src/main/resources/static/scripts/components/navbar.js
@@ -1,6 +1,6 @@
 function NavBar() {
   return (
-  <nav class="navbar navbar-expand-lg bg-primary"data-bs-theme="dark">
+  <nav class="navbar navbar-expand-sm bg-primary sticky-top" data-bs-theme="dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="./index.html">Tutor Bridge</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,18 +9,20 @@ function NavBar() {
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
+            <a class="nav-link" href="./listings.html">Search Tutors</a>
           </li>
+          
+        </ul>
+        <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link" href="./login.html">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./login.html">Register</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./listings.html">Listings</a>
+            <div class="btn-group">
+              <a class="btn btn-outline-light" href="./login.html">Login</a>
+              <a class="btn btn-outline-light" href="./login.html">Register</a>
+            </div>
           </li>
         </ul>
+
+        
       </div>
     </div>
   </nav>
