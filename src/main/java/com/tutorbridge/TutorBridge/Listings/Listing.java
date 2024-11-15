f708bbfb004f6c35f84137bba9db81d59740ee86
Gabriel-Munoz <g.munoz1@ufl.edu>
2024-11-15 11:25:42 -0500

    Edited Listing Database to Match Emma's set parameters

--- src/main/java/com/tutorbridge/TutorBridge/Listings/Listing.java
+++ src/main/java/com/tutorbridge/TutorBridge/Listings/Listing.java
@@ -10,7 +10,9 @@ public class Listing {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    private String title;
+    private String classes;
+    private String contact;
+    private String additionalInfo;
     private String description;
     private double price;
 
@@ -21,17 +23,34 @@ public class Listing {
 
     public Listing() {}
 
-    public Listing(String title, String description, double price, User user) {
-        this.title = title;
+    public Listing(String classes, String description,String contact,String additionalInfo, double price, User user) {
+        this.classes = classes;
+        this.contact = contact;
+        this.additionalInfo = additionalInfo;
         this.description = description;
         this.price = price;
         this.user = user;
     }
 
+    public Listing(String classes, String description,String contact, double price, User user) {
+        this.classes = classes;
+        this.contact = contact;
+        this.additionalInfo = null;
+        this.description = description;
+        this.price = price;
+        this.user = user;
+    }
+    
     // Getters and setters
     public int getId() { return id; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public String getClasses() { return classes; }
+    public void setClasses(String Class) { this.classes = Class; }
+
+    public String getContact() { return contact; }
+    public void setContact(String Contact) { this.contact = Contact; }
+
+    public String getAdditionalInfo() { return additionalInfo; }
+    public void setAdditionalInfo(String AdditionalInfo) { this.additionalInfo = AdditionalInfo; }
 
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
